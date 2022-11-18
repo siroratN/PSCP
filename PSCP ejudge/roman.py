74213434ffd567b39918507e4c55228b2bb06de1
siroratnambun <65070217@kmitl.ac.th>
2022-11-18 16:27:36 +0700

    roman

---
+++ PSCP ejudge/roman.py
@@ -0,0 +1,13 @@
+""" roman """
+def main():
+    """ roman """
+    maps = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+    value = input()
+    result = 0
+    for i, j in enumerate(value):
+        if (i + 1) == len(value) or maps[j] >= maps[value[i+1]]:
+            result += maps[j]
+        else:
+            result -= maps[j]
+    print(result)
+main()
